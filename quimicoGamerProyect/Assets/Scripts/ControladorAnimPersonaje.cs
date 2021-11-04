e608b0475b9956f14b46b3ce69495b42d7907b2a
Xime Villarejo <ximvc99@gmail.com>
2021-11-04 14:04:44 -0600

    mix

--- quimicoGamerProyect/Assets/Scripts/ControladorAnimPersonaje.cs
+++ quimicoGamerProyect/Assets/Scripts/ControladorAnimPersonaje.cs
@@ -18,28 +18,31 @@ public class ControladorAnimPersonaje : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        if (horizontalInput != 0)
-        {
-            if (horizontalInput < 0)
-            {
-                horizontalInput = (horizontalInput) * (-1);
-            }
+        //if (horizontalInput != 0)
+        //{
+        //    if (horizontalInput < 0)
+        //    {
+        //        horizontalInput = (horizontalInput) * (-1);
+        //    }
             animatorPersonaje.SetFloat("JoystickAxis", horizontalInput);
-        }
-        if (verticalInput != 0)
-        {
-            if (verticalInput < 0)
-            {
-                verticalInput = (verticalInput) * (-1);
-            }
-            animatorPersonaje.SetFloat("JoystickAxis", verticalInput);
-        }
-        if (Input.GetAxis("Jump") > 0)
-        {
-            animatorPersonaje.SetTrigger("attack");
-        }
+        //}
+        //if (verticalInput != 0)
+        //{
+        //    if (verticalInput < 0)
+        //    {
+        //        verticalInput = (verticalInput) * (-1);
+        //    }
+            animatorPersonaje.SetFloat("JoystickAxis2", verticalInput);
+        //}
+        animatorPersonaje.SetLayerWeight(1, Input.GetAxis("Jump"));
 
-    }
+        //if (Input.GetAxis("Jump") > 0)
+        //{
+        //    animatorPersonaje.SetLayerWeight(1, Input.GetAxis("Jump"));
+        //    animatorPersonaje.SetTrigger("attack");
+        //}
+        
+}
 
     public void Pisada()
     {
