5b20440087175ebecc43855e220ced80c570abda
Xime Villarejo <ximvc99@gmail.com>
2021-11-04 13:12:56 -0600

    sonidos2

--- quimicoGamerProyect/Assets/Scripts/ControladorAnimZombie.cs
+++ quimicoGamerProyect/Assets/Scripts/ControladorAnimZombie.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ControladorAnimZombie : MonoBehaviour
 {
+    public Animator animatorPersonaje;
+    public AudioSource fuenteAudio;
+    public AudioClip sonidoPasos;
+    public float PlayerSpeed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,10 @@ public class ControladorAnimZombie : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Pisada()
+    {
+        Debug.Log("piso");
+        fuenteAudio.PlayOneShot(sonidoPasos);
     }
 }
