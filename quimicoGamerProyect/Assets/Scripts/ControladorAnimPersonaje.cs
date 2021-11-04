58965f84ed1826aaaf5879afa58339653bcdd66d
Xime Villarejo <ximvc99@gmail.com>
2021-11-04 12:34:00 -0600

    blendtree terminado

--- quimicoGamerProyect/Assets/Scripts/ControladorAnimPersonaje.cs
+++ quimicoGamerProyect/Assets/Scripts/ControladorAnimPersonaje.cs
@@ -5,25 +5,48 @@ using UnityEngine;
 public class ControladorAnimPersonaje : MonoBehaviour
 {
     public Animator animatorPersonaje;
-    public AudioSource fuenteAudio;
-    public AudioClip sonidoPasos;
 
-    private bool hasWeapon;
+    //public AudioSource fuenteAudio;
+    //public AudioClip sonidoPasos;
 
+    public float PlayerSpeed = 5;
     // Start is called before the first frame update
     void Start()
     {
-        hasWeapon = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (horizontalInput != 0)
+        {
+            if (horizontalInput < 0)
+            {
+                horizontalInput = (horizontalInput) * (-1);
+            }
+            animatorPersonaje.SetFloat("JoystickAxis", horizontalInput);
+        }
+        if (verticalInput != 0)
+        {
+            if (verticalInput < 0)
+            {
+                verticalInput = (verticalInput) * (-1);
+            }
+            animatorPersonaje.SetFloat("JoystickAxis", verticalInput);
+        }
+        if (Input.GetAxis("Jump") > 0)
+        {
+            animatorPersonaje.SetTrigger("attack");
+        }
+
     }
 
     public void Pisada()
     {
-        fuenteAudio.PlayOneShot(sonidoPasos);
+        Debug.Log("ok");
+        //fuenteAudio.PlayOneShot(sonidoPasos);
     }
 }
