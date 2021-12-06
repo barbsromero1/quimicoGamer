34ffaf6b97032364c94d0502c4fe30de0a1d12a1
Xime Villarejo <ximvc99@gmail.com>
2021-12-05 19:11:30 -0600

    timer

--- quimicoGamerProyect/Assets/Timer.cs
+++ quimicoGamerProyect/Assets/Timer.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float timeValue = 90;
-    public TextMeshPro timeText;
+    public float timeValue = 300;
+    public Text timeText;
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +21,18 @@ public class Timer : MonoBehaviour
         if (timeValue > 0)
         {
             timeValue -= Time.deltaTime;
+            if (timeValue < 31)
+            {
+                timeText.color = Color.magenta;
+            }
         }
         else
         {
             //game over
-            timeValue += 90;
+            timeValue += 300;
         }
 
-        
+        DisplayTime(timeValue);
     }
 
     void DisplayTime (float timeDisplay)
