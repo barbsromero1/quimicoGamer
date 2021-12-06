45e419476475aa9b789100fb9c802fb576417351
Xime Villarejo <ximvc99@gmail.com>
2021-12-05 18:53:27 -0600

    ui/timer

---
+++ quimicoGamerProyect/Assets/Timer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeValue = 90;
+    public TextMeshPro timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            //game over
+            timeValue += 90;
+        }
+
+        
+    }
+
+    void DisplayTime (float timeDisplay)
+    {
+        if(timeDisplay < 0)
+        {
+            timeDisplay = 0;
+        }
+        float minutes = Mathf.FloorToInt(timeDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeDisplay % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
